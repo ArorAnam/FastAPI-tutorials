6a5c76f5fe5ff0bf9b88c23f637e3ad069881fad
Naman Arora <naarora@tcd.ie>
2020-07-12 03:17:17 +0530

    done Forms and Custom response

--- requestFile.py
+++ requestFile.py
@@ -10,4 +10,4 @@ async def create_file(file: bytes = File(...)):
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    return {"filename": file.filename} 
